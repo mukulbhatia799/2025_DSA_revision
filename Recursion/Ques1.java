dbdc48bc2bbeffdae7958c78edd650071ea774af
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-01 19:10:38 +0530

    done

--- Recursion/Ques1.java
+++ Recursion/Ques1.java
@@ -1,19 +1,22 @@
-// last occ of a char
+// last occ of a char RE
 import java.util.Scanner;
 public class Ques1 {
+    public static int findAnsRE(String str, char ch, int ind) {
+        if(ind == -1) return -1;
+
+        if(str.charAt(ind) == ch) return ind;
+
+        return findAnsRE(str, ch, ind-1);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
-        char ch = sc.next().charAt(0);
+        char ch = sc.next().charAt(0);   
+        
+        int ind = findAnsRE(str, ch, str.length()-1);
 
-        for(int i = str.length()-1; i >= 0; i--) {
-            if(str.charAt(i) == ch) {
-                System.out.println("Last Occ index is " + i);
-                return;
-            }
-        }
-
-        System.out.println("Not found!!");
+        if(ind == -1) System.out.println("Not found!!"); 
+        else System.out.println("found at " + ind);
     }
 }
