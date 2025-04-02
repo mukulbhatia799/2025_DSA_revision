8ef8a33234e62e76db77640080e9012b3495be17
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-02 22:06:41 +0530

    done

--- Recursion/Ques4.java
+++ Recursion/Ques4.java
@@ -1,7 +1,26 @@
+
+// palindrome check RE
+
 import java.util.Scanner;
 
 public class Ques4 {
+    public static boolean findAnsRE(String str, int i, int j) {
+        if(i >= j) return true;
+        if(str.charAt(i) != str.charAt(j)) return false;
+
+        return findAnsRE(str, i+1, j-1);
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        boolean check = findAnsRE(str, 0, str.length()-1);
+
+        if(check == true) {
+            System.out.println("palindrome");
+        }
+        else {
+            System.out.println("not a palindrome");
+        }
     }
 }
