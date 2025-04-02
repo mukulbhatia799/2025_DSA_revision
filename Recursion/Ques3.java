d8f73ff26a10e6ea3ff13acaf1a533a9b3aec1b6
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-02 21:48:54 +0530

    done

--- Recursion/Ques3.java
+++ Recursion/Ques3.java
@@ -5,39 +5,43 @@ import java.util.Scanner;
 public class Ques3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         String num1 = sc.next();
         String num2 = sc.next();
 
-        int n = num1.length()-1, m = num2.length()-1, carry = 0;
+        int i = num1.length()-1, j = num2.length()-1, carry = 0;
 
-        StringBuilder newstr = new StringBuilder();
-        while(n >= 0 && m >= 0) {
-            int val1 = num1.charAt(n)-'0';
-            int val2 = num2.charAt(m)-'0';
-            
-            newstr.append(val1+val2+carry);
-            carry = (val1+val2)/10;
-            n--; m--;
+        StringBuilder ans = new StringBuilder();
+        while(i >= 0 && j >= 0) {
+            int val1 = num1.charAt(i)-'0';
+            int val2 = num2.charAt(j)-'0';
+
+            int sum = val1+val2+carry;
+            carry = sum / 10;
+            ans.append(sum%10);
+
+            i--; j--;
+        }
+        System.out.println("before: " + i + " " + j);
+        while(i >= 0) {
+            int sum = (num1.charAt(i)-'0') + carry;
+            carry = sum/10;
+            ans.append(sum%10);
+            i--;
         }
 
-        while(n >= 0) {
-            int val = num1.charAt(n)-'0';
-            newstr.append(val+carry);
-            carry = 0;
-            n--;
+        while(j >= 0) {
+            int sum = (num2.charAt(j)-'0') + carry;
+            carry = sum/10;
+            ans.append(sum%10);
+            j--;
         }
 
-        while(m >= 0) {
-            int val = num2.charAt(m)-'0';
-            newstr.append(val+carry);
-            carry = 0;
-            m--;
-        }
+        if(carry > 0) ans.append(carry);
 
-        newstr.reverse();
+        ans.reverse();
 
-        for(char ch: newstr.toString().toCharArray()) {
-            System.out.print(ch);
-        }
+        System.out.println(ans);
+        
     }
 }
