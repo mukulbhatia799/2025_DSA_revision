fda07b7c615514c05e189b1bd85dfb9726ce2975
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-02 22:01:56 +0530

    done RE

--- Recursion/Ques3.java
+++ Recursion/Ques3.java
@@ -3,6 +3,30 @@
 
 import java.util.Scanner;
 public class Ques3 {
+    public static void findAnsRE(String num1, String num2, StringBuilder ans, int i, int j, int carry) {
+        if(i < 0 && j < 0) {
+            if(carry > 0) ans.append(carry);
+            return;
+        }
+
+        if(i >= 0 && j >= 0) {
+            int sum = ((num1.charAt(i)-'0') + (num2.charAt(j)-'0') + carry);
+            carry = sum / 10;
+            ans.append(sum%10);
+        }
+        else if(i >= 0) {
+            int sum = ((num1.charAt(i)-'0') + carry);
+            carry = sum / 10;
+            ans.append(sum%10);
+        }
+        else if(j >= 0) {
+            int sum = ((num2.charAt(j)-'0') + carry);
+            carry = sum / 10;
+            ans.append(sum%10);
+        }
+
+        findAnsRE(num1, num2, ans, i-1, j-1, carry);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -12,36 +36,10 @@ public class Ques3 {
         int i = num1.length()-1, j = num2.length()-1, carry = 0;
 
         StringBuilder ans = new StringBuilder();
-        while(i >= 0 && j >= 0) {
-            int val1 = num1.charAt(i)-'0';
-            int val2 = num2.charAt(j)-'0';
-
-            int sum = val1+val2+carry;
-            carry = sum / 10;
-            ans.append(sum%10);
-
-            i--; j--;
-        }
-        System.out.println("before: " + i + " " + j);
-        while(i >= 0) {
-            int sum = (num1.charAt(i)-'0') + carry;
-            carry = sum/10;
-            ans.append(sum%10);
-            i--;
-        }
-
-        while(j >= 0) {
-            int sum = (num2.charAt(j)-'0') + carry;
-            carry = sum/10;
-            ans.append(sum%10);
-            j--;
-        }
-
-        if(carry > 0) ans.append(carry);
+        
+        findAnsRE(num1, num2, ans, i, j, carry);
 
         ans.reverse();
-
         System.out.println(ans);
-        
     }
 }
