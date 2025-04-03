a32a55a3ae33190f5df8ca156949f99f006a64fb
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-04 00:43:26 +0530

    done

--- Recursion/Ques9.java
+++ Recursion/Ques9.java
@@ -1,25 +1,38 @@
 public class Ques9 {
     public static boolean findAns(String s, String p, int si, int pi) {
         if(si == s.length() && pi == p.length()) return true;
-        else if(si >= s.length() || pi >= p.length()) return false;
+        else if(si == s.length()) {
+            while(pi < p.length() && p.charAt(pi) == '*') pi++;
+
+            if(pi == p.length()) return true;
+            return false;
+        }
+        else if(pi == p.length()) return false;
 
         if(s.charAt(si) == p.charAt(pi) || p.charAt(pi) == '?') {
             return findAns(s, p, si+1, pi+1);
         }
-        else if(p.charAt(pi) == '*') {  // else is for '*'
-            boolean asNull = findAns(s, p, si, pi+1);        // let * as null;
+        else if(p.charAt(pi) == '*') {
+            while(pi < p.length()-1 && p.charAt(pi+1) == '*') pi++;
+            // let * as null;
+            boolean letNull = findAns(s, p, si, pi+1);
 
-            boolean pickChar = findAns(s, p, si+1, pi);        // let * equals s[si] but still it can take others.
+            // let * equals s[si] but don't move pi as * could be matched others too.
+            boolean matchChar = findAns(s, p, si+1, pi);
 
-            return asNull || pickChar;
+            return letNull || matchChar;
         }
-        else return false;  // when p[pi] != s[si] and p[pi] is an alphabet.
+        else {  // when s[si] != p[si] and neither p[si] is '?' nor '*'
+            return false;
+        }
     }
     public static void main(String[] args) {
         String s = "abcdefg";
         // String p = "ab*???fg";
         String p = "ab*fg";
         // String p = "ab*????fg";
+        // String p = "ab*****";
+        // String p = "ab*****pq";
 
         boolean ans = findAns(s, p, 0, 0);
 
