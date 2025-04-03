12ad40724d2081e6ae99379732b51370b4ac1a7d
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-03 18:11:05 +0530

    done

---
+++ Recursion/Ques8.java
@@ -0,0 +1,20 @@
+// house robber problem
+import java.util.Scanner;
+
+public class Ques8 {
+    public static int findAns(int[] nums, int ind) {
+        if(ind >= nums.length) return 0;
+
+        int pick = nums[ind] + findAns(nums, ind+2);
+        int notPick = findAns(nums, ind+1);
+
+        return Math.max(pick, notPick);
+    }
+    public static void main(String[] args) {
+        // int nums[] = {1,2,3,1};
+        int nums[] = {2,7,9,3,1};
+        int ans = findAns(nums, 0);
+
+        System.out.println("Max could be robbed: " + ans);
+    }
+}
