2c6c153ebf1b598182561d5aa935368b43946cf9
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-07 01:05:15 +0530

    done

---
+++ BacktrackingAndDnC/Ques7.java
@@ -0,0 +1,89 @@
+// beautiful arrangement
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// ====== using permutation method gives tle - t.c -> O(n!) ====== 
+
+// public class Ques7 {
+//     public static void findAllPerm(List<Integer> list, int ind, int[] count) {
+//         if(ind >= list.size()) {
+//             boolean check = true;
+//             for(int i = 0; i < list.size(); i++) {
+//                 if(list.get(i)%(i+1) != 0 && (i+1)%list.get(i) != 0) {
+//                     check = false;
+//                     break;
+//                 }
+//             }
+
+//             if(check == true) {
+//                 count[0]++;
+//             }
+
+//             return;
+//         }
+
+//         for(int i = ind; i < list.size(); i++) {
+//             Collections.swap(list, i, ind);
+
+//             findAllPerm(list, ind+1, count);
+
+//             Collections.swap(list, i, ind);
+//         }
+//     }
+//     public static int countArrangement(int n) {
+//         List<Integer> list = new ArrayList<>();
+//         for(int i = 1; i <= n; i++) {
+//             list.add(i);
+//         }
+
+//         int[] count = new int[1];
+//         findAllPerm(list, 0, count);
+//         return count[0];
+//     }
+//     public static void main(String[] args) {
+//         int n = 3;
+
+//         int count = countArrangement(n);
+
+//         System.out.println("count: " + count);
+//     }
+// }
+
+
+
+//  ============ using backtracking method ============ 
+
+public class Ques7 {
+    public static void findAns(int[] vis, int n, int[] count, int currValue) {
+        if(currValue == n+1) {
+            count[0]++;
+            return ;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(vis[i] == 0 && (currValue % i == 0 || i % currValue == 0)) {
+                vis[i] = currValue;
+                findAns(vis, n, count, currValue+1);
+                vis[i] = 0;
+            }
+        }
+    }
+    public static int countArrangement(int n) {
+        int[] vis = new int[n+1];
+
+        int[] count = new int[1];
+        int currValue = 1;
+        findAns(vis, n, count, currValue);
+
+        return count[0];
+    }
+    public static void main(String[] args) {
+        int n = 15;
+
+        int count = countArrangement(n);
+
+        System.out.println("count: " + count);
+    }
+}
