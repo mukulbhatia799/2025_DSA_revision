5c0e77a05ac9e8ae52ebd3d7a47977af8fef0699
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-11 17:44:52 +0530

    done

--- LinkedList/Ques9.java
+++ LinkedList/Ques9.java
@@ -1,5 +1,32 @@
 // Merge nodes in between zeros.
 
+class ListNode {
+    int val;
+    ListNode next;
+}
+
 public class Ques9 {
-    
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null) return head;
+        int count = 0;
+
+        ListNode firstList = head, secondList = head.next;
+        ListNode secondListHeadTemp = secondList;
+
+        while(firstList.next != null && secondList.next != null) {
+            if((count & 1) == 1) {
+                secondList.next = firstList.next;
+                secondList = secondList.next;
+            }
+            else {
+                firstList.next = secondList.next;
+                firstList = firstList.next;
+            }
+            count++;
+        }
+        if(firstList.next == null) secondList.next = null;
+        firstList.next = secondListHeadTemp;
+
+        return head;
+    }
 }
