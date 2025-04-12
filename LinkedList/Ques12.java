4dd9615db83b6ae372774ca36b0d8d7a4e14fdc7
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-04-12 12:34:17 +0530

    done

--- LinkedList/Ques12.java
+++ LinkedList/Ques12.java
@@ -2,40 +2,78 @@
 
 
 // O(n^2) time complexity.
-public class Ques12 {
-    // public void print(ListNode head) {
-    //     while(head != null) {
-    //         System.out.print(head.val + " ");
-    //         head = head.next;
-    //     }
-    //     System.out.println();
-    // }
-    public ListNode removeZeroSumSublists(ListNode head) {
-        ListNode curr = head, prev = null;
+// public class Ques12 {
 
-        while(curr != null) {
-            ListNode temp2 = curr;
-            int sum = 0;
-            while(temp2 != null) {
-                sum += temp2.val;
-                if(sum == 0) break;
-                temp2 = temp2.next;
+//     public ListNode removeZeroSumSublists(ListNode head) {
+//         ListNode curr = head, prev = null;
+
+//         while(curr != null) {
+//             ListNode temp2 = curr;
+//             int sum = 0;
+//             while(temp2 != null) {
+//                 sum += temp2.val;
+//                 if(sum == 0) break;
+//                 temp2 = temp2.next;
+//             }
+//             if(sum == 0) {
+//                 if(prev == null) {
+//                     curr = head = temp2.next;
+//                 }
+//                 else {
+//                     prev.next = temp2.next;
+//                     curr = temp2.next;
+//                     temp2.next = null;
+//                 }
+//             }
+//             else {
+//                 prev = curr;
+//                 curr = curr.next;
+//             }
+//         }
+
+//         return head;
+//     }
+// }
+
+
+// Approach 2: Prefix Sum Hash Table - (Optimized solution)
+
+
+import java.util.HashMap;
+
+class ListNode {
+    int val;
+    ListNode next;
+}
+
+class Solution {
+    public void sanitizeMap(HashMap<Integer, ListNode> hmap, ListNode start, ListNode end, int pfSum) {
+        while(start != end) {
+            pfSum += start.val;
+            hmap.remove(pfSum);
+            start = start.next;
+        }
+    }
+    public ListNode removeZeroSumSublists(ListNode head) {
+        HashMap<Integer, ListNode> hmap = new HashMap<>();
+
+        int pfSum = 0;
+        ListNode temp = head;
+        while(temp != null) {
+            pfSum += temp.val;
+            if(pfSum == 0) {
+                head = temp.next;
+                hmap.clear();
             }
-            if(sum == 0) {
-                if(prev == null) {
-                    curr = head = temp2.next;
-                }
-                else {
-                    prev.next = temp2.next;
-                    curr = temp2.next;
-                    temp2.next = null;
-                }
+            else if(hmap.containsKey(pfSum)) {
+                ListNode temp2 = hmap.get(pfSum);
+                sanitizeMap(hmap, temp2.next, temp, pfSum);
+                temp2.next = temp.next;
             }
             else {
-                prev = curr;
-                curr = curr.next;
+                hmap.put(pfSum, temp);
             }
-            // print(head);
+            temp = temp.next;
         }
 
         return head;
