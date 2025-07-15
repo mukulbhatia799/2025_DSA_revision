f4f852158e2df8914f2165e1197b562badf52934
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-07-15 12:34:40 +0530

    dp approach - find subset with sum k

--- DP_Workshop/5.cpp
+++ DP_Workshop/5.cpp
@@ -18,28 +18,35 @@
 #include<vector>
 using namespace std;
 
-bool solve(int n, int t, int arr[], int ptr, int currSum) {
+
+bool solve(int n, int t, int arr[], vector<vector<int>> &dp, int ptr, int currSum) {
     if(ptr == n) {
         if(currSum == t) return true;
         return false;
     }
 
+    if(dp[ptr][currSum] != -1) {
+        return dp[ptr][currSum];
+    }
+
     bool take = false;
     // take
-    if(currSum+arr[ptr] <= t) take = solve(n, t, arr, ptr+1, currSum+arr[ptr]);
+    if(currSum+arr[ptr] <= t) take = solve(n, t, arr, dp, ptr+1, currSum+arr[ptr]);
 
     // not take
-    bool notTake = solve(n, t, arr, ptr+1, currSum);
-    return (take || notTake);
+    bool notTake = solve(n, t, arr, dp, ptr+1, currSum);
+    return dp[ptr][currSum] = (take || notTake);
 }
 
 int main() {
-    int n = 4, t = 4;
+    // int n = 4, t = 16;
+    int n = 5, t = 4;
 
-    int arr[n] = {1, 2, 5, 9};
+    // int arr[n] = {1, 2, 5, 9};
+    int arr[n] = {1, 1, 1, 1, 1};
+    vector<vector<int>> dp(n+1, vector<int>(t+1, -1));
 
-    cout << "Result: " << solve(n, t, arr, 0, 0);
-    
+    cout << "Result: " << solve(n, t, arr, dp, 0, 0) << endl;    
 
     return 0;
 }
